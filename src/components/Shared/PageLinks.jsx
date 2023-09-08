863dda6560b0ddeabcf3288febce459a9e4c5aa0
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-09-08 21:32:34 +0530

    refactor: Page Links Changed

--- src/components/Shared/PageLinks.jsx
+++ src/components/Shared/PageLinks.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
 const PageLinks = () => {
-    const links = ['About', 'Career', 'Achievements', 'Contact'];
+    const links = ['About', 'Our Journey', 'Our Team', 'Contact'];
 
     return (
         <ul className="page__links">
             {links.map(link => (
                 <li className="page__link" key={link}>
-                    <Link to={`/${link}`}>{link.toLowerCase()}</Link>
+                    <Link to={`/${link}`}>{link.toLowerCase}</Link>
                 </li>
             ))}
         </ul>
