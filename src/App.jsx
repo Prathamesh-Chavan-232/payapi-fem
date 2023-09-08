b8a29aa47349740be085849ff793c75deb501215
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-09-08 13:50:29 +0530

    feat: PayAPI design

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Pricing, About, Contact } from './mainPages';
+import { Navbar, CTA, Footer } from './components';
+import './App.scss';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/pricing" element={<Pricing/>} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
+      <CTA />
+      <Footer />
+    </div>
+</Router>
+);
+
+export default App;
---
+++ src/mainPages/index.jsx
@@ -0,0 +1,8 @@
+import Home from "./Home.jsx"
+import About from "./About.jsx"
+import Pricing from "./Pricing.jsx"
+import Contact from "./Contact.jsx"
+export { default as Home } from './Home';
+export { default as Pricing } from './Pricing';
+export { default as About } from './About';
+export { default as Contact } from './Contact';
