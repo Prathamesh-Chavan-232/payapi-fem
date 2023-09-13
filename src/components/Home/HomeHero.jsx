751a8cf43387a7f970755d0e0fffab9e73543f97
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-09-13 21:33:34 +0530

    ui: Hero section

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
-import { DemoForm } from '../';
 import phone from '../../images/home/desktop/illustration-phone-mockup.svg';
 
 const HomeHero = () => (
     <div className="hero">
         <div className="hero__textbox">
             <h1 className="hero__textbox__heading">
-                Unwrapping Smiles: Where Happiness Gets Delivered</h1>
-            <DemoForm />
+                Unwrapping Smiles: Where Happiness Gets Delivered
+            </h1>
+            <span className='hero__textbox__subheading'>
+                {"At Hectec Solutions, we're not just a company – we're your go-to partner for all things shopping! Established in September 2017, we've been on an incredible journey, bringing smiles to countless households across India."}
+            </span>
             <span className="hero__textbox__contact-us">
                 Have any questions? <Link to="/contact">Contact us</Link>
             </span>
