3a62ec27a24c1fc80cd477bc5d747315d66f11ed
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-09-13 11:37:27 +0530

    fix: footer

--- src/mainPages/Contact.jsx
+++ src/mainPages/Contact.jsx
@@ -2,11 +2,11 @@ import { Form, PartnersGrid } from '../components';
 
 const Contact = () => (
     <div className="page contact"> <h1 className="contact__heading">
-            Submit a help request and we’ll get in touch shortly.
-        </h1>
+        Submit a help request and we’ll get in touch shortly.
+    </h1>
         <main className="contact__main">
             <Form />
-    {/*
+            {/*
             <div className="contact__investors__grid">
                 <h3 className="contact__investors__grid__heading">Join the thousands of innovators already building with us</h3>
         <PartnersGrid white={true} />
