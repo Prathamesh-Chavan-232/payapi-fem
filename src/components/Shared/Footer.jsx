2c3d470450ed60db0f4869bfcb0d4261be5093fb
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-09-26 03:14:36 +0530

    refactor: resolved duplicate imports

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { PageLinks } from '../';
-<<<<<<< HEAD
-import logo from '../../images/shared/desktop/logo-footer.svg'
-=======
-import logo from '../../images/shared/desktop/logo--white.svg';
->>>>>>> f637018 (refactor: changed UI)
+import { Link } from "react-router-dom";
+import { PageLinks } from "../";
+import logo from "../../images/shared/desktop/logo--white.svg";
 
 const Footer = () => (
-    <div className="footer">
-        <Link to="/">
-            <img className="footer__logo" src={logo} alt="PayAPI company logo" />
-        </Link>
-        <PageLinks />
-        <div className="footer__socials">
-            <a href="#" aria-label="Facebook icon">
-                <i className="fab fa-facebook-square" />
-            </a>
-            <a href="#" aria-label="Twitter icon">
-                <i className="fab fa-twitter" />
-            </a>
-            <a href="#" aria-label="LinkedIn icon">
-                <i className="fab fa-linkedin" />
-            </a>
-        </div>
-    </div>
-)
+	<div className='footer'>
+		<Link to='/'>
+			<img
+				className='footer__logo'
+				src={logo}
+				alt='PayAPI company logo'
+			/>
+		</Link>
+		<PageLinks />
+		<div className='footer__socials'>
+			<a
+				href='#'
+				aria-label='Facebook icon'
+			>
+				<i className='fab fa-facebook-square' />
+			</a>
+			<a
+				href='#'
+				aria-label='Twitter icon'
+			>
+				<i className='fab fa-twitter' />
+			</a>
+			<a
+				href='#'
+				aria-label='LinkedIn icon'
+			>
+				<i className='fab fa-linkedin' />
+			</a>
+		</div>
+	</div>
+);
 
 export default Footer;
